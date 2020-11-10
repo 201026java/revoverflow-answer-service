0583f7c8ae2c36db7272b4d7b2080bb3d4628ea2
Hugh Thornhill <hlowerthornhill@gmail.com>
2020-11-10 18:06:22 -0500

    Adding test suite to answer service

---
+++ src/test/java/com/revature/AllTests.java
@@ -0,0 +1,11 @@
+package com.revature;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ AnswerControllerTest.class, AnswerServiceTest.class })
+public class AllTests {
+
+}
