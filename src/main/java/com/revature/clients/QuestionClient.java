26622a1ab88f51db9f5d335e24bbbb8848abeeb6
Mark Alsip <mark.alsip@comcast.net>
2020-11-12 12:42:41 -0700

    I added comments to my code

--- src/main/java/com/revature/clients/QuestionClient.java
+++ src/main/java/com/revature/clients/QuestionClient.java
@@ -13,6 +13,13 @@ import com.revature.models.Question;
 @RequestMapping("/question")
 public interface QuestionClient {
 
+	/** @Author Mark Alsip
+	 * Fetches filtered questions from the questions service.
+	 * Since openfeign doesn't like page or pageable a new endpoint
+	 * just for this operation was made in question controller.
+	 * 
+	 * @return note that it returns a list, not a page.
+	 */
 	@GetMapping("/non-paged/filter")
 	public List<Question> getAllQuestionsByFilter(@RequestParam(value="questionType") String questionType, @RequestParam(value="location") String location, @RequestParam(value="id") int id);
 	
