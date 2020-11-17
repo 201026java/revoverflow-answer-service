b1455fe374d10c95c2c6b8b474c38745287a71d3
Hugh Thornhill <hlowerthornhill@gmail.com>
2020-11-17 13:08:13 -0500

    Service and controller tests are passing with mock users added

--- src/test/java/com/revature/AnswerControllerTest.java
+++ src/test/java/com/revature/AnswerControllerTest.java
@@ -1,6 +1,12 @@
 package com.revature;
 
 import static org.hamcrest.CoreMatchers.is;
+<<<<<<< HEAD
+=======
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
+import static org.junit.Assert.assertTrue;
+>>>>>>> 643c4cf... Service and controller tests are passing with mock users added
 import static org.mockito.Mockito.when;
 import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
