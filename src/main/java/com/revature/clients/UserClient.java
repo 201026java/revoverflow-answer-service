08e0202e36af89082ab0d3ef11433102746d4638
bukadirit <bukadiri.trawally@mail.citytech.cuny.edu>
2020-11-18 14:00:14 -0500

    Troubleshooting feign client

--- src/main/java/com/revature/clients/UserClient.java
+++ src/main/java/com/revature/clients/UserClient.java
@@ -10,7 +10,7 @@ import com.revature.models.User;
 @FeignClient(name = "RevOverflow-UserService")
 public interface UserClient {
 
-	@PostMapping("/authorize")
+	@PostMapping("/user/authorize")
 	public User authUser(User user);
 	
 }
