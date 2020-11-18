97f0ed9211b88b0bab3245887b6f37d601bc3335
bukadirit <bukadiri.trawally@mail.citytech.cuny.edu>
2020-11-18 10:51:45 -0500

    Fixed test issues.

--- src/test/java/com/revature/AnswerControllerTest.java
+++ src/test/java/com/revature/AnswerControllerTest.java
@@ -17,7 +17,6 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.Before;
-//import org.junit.jupiter.api.Test;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mockito;
